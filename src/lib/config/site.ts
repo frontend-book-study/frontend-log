3b15997386352d530f54b605c4b931ccae74375c
choisohyun <sohyun4283@naver.com>
2024-07-20 19:14:30 +0900

    fix: protocol 기본을 https로 변경

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
-  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ?? 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
   title: 'Frontend Log',
   subtitle: '프론트엔드 책 공부하는 블로그',
